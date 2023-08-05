c2c833566692875a99f5cd82286f889369de8a1c
Jossue <jossuearteche000@gmail.com>
2023-08-05 02:34:09 -0400

    desactivado el operador de asignacion para evitar bugs

--- Hulk/Parser.cs
+++ Hulk/Parser.cs
@@ -79,6 +79,9 @@ namespace Hulk
                         else
                             Vars.Add(exp as Variable);
                     }
+                    //comentar la siguiente linea para que el operador de asignacion funcione. lo desactive porque aun no funciona bien
+                    //al mezclarlo con otras operaciones (3 + a:=b por ejemplo da bateo)
+                    return null;  
                     return new Asignment(Vars, right);
                 }
             }
@@ -311,7 +314,7 @@ namespace Hulk
             HulkExpression ValueExp = null;
             if (declarationEnd < end - 1)
                 ValueExp = ParseInternal(tokens, declarationEnd + 2, end);
-            else if (declarationEnd == end - 1)
+            else if (declarationEnd == end - 1 || declarationEnd > end - 1) //comentar miembro derecho del or para poder declarar varibales no inicializadas
                 throw new SyntaxError("value expression", "variable declaration");
             result = new VariableDeclaration(names, type, ValueExp);
             return result;
