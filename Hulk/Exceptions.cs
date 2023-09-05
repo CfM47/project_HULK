d1ef51a780781b7031e6b2f4746d1c3cbcd80b16
Jossue <jossuearteche000@gmail.com>
2023-09-05 11:42:21 -0400

    Cambiado el mensaje de las OperationSemanticError de "Operation `{operation}` cannot be used between  `{leftArg}` and `{rightArg}`" a "Operator `{operation}` cannot be applied to operands of type  `{leftArg}` and `{rightArg}`"

--- Hulk/Exceptions.cs
+++ Hulk/Exceptions.cs
@@ -87,9 +87,9 @@ namespace Hulk
 			LeftArgument = leftArg;
 			RightArgument = rightArg;
 			Expected = expected;
-			MessageDefinition = $"Operation `{Operation}` cannot be used between `{leftArg}` and `{rightArg}`";
-		}
-		public string Operation { get; }
+            MessageDefinition = $"Operator `{operation}` cannot be applied to operands of type  `{leftArg}` and `{rightArg}`";
+        }
+        public string Operation { get; }
 		public string LeftArgument { get; }
 		public string RightArgument { get; }
 		public string Expected { get; }
